3069ae86d1bfa5ff6cb54ba0d73c07344bc94f28
Roudolphe <legrandfute13@gmail.com>
2023-01-04 13:50:59 +0100

    consultant blockchain

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 
     //saisie animation
     var typed = new Typed(".typing", {
-        strings: ["Blockchain Developper", "Web Developper", "Designer", "Freelancer"],
+        strings: ["Blockchain Developper", "Web Developper", "Web Designer", "Consultant Blockchain", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop:true
